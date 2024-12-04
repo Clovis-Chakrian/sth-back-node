bd00c3c1d6e98dc445e87387ffa0e4a49f03fc90
GustavoFelips <130930887+GustavoFelips@users.noreply.github.com>
2024-12-04 12:04:57 +0000

    refactor: removendo o teste

--- src/application/services/respostaService.js
+++ src/application/services/respostaService.js
@@ -1,16 +1,9 @@
 import { RespostaRepository } from "../../infra/repositories/respostaRepository.js";
-import { Resposta } from "../../domain/entities/resposta.js";
 
 const respostaRepository = new RespostaRepository();
 
 const respostaService = {
   async getAll() {
-     // Realiza a consulta ao banco
-     //const respostas = await Resposta.findAll();
-     //console.log('Respostas no Service:', respostas);
-
-     // Se encontrar respostas, retorna elas, senão retorna um array vazio
-     //return respostas || [];
      return await respostaRepository.findAll();
   },
 
