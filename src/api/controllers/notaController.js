3352fe516bc776a58ae2b7027daadc1c5045895a
victor.costa213 <132701394+victorcosta213@users.noreply.github.com>
2024-12-05 11:54:56 -0300

    fix: adaptando ao tratamento de erro

--- src/api/controllers/notaController.js
+++ src/api/controllers/notaController.js
@@ -3,48 +3,48 @@ import { notaService } from "../../application/services/notaService.js";
 
 const notaController = Router();
 
-notaController.get("/", async (req, res) => {
+notaController.get("/", async (req, res, next) => {
   try {
     const notas = await notaService.getAll();
     res.status(200).json(notas);
   } catch (error) {
-    res.status(500).json({ error: "Erro ao listar notas!", message: error.message });
+    next(error); 
   }
 });
 
-notaController.get("/:id", async (req, res) => {
+notaController.get("/:id", async (req, res, next) => {
   try {
     const nota = await notaService.getById(req.params.id);
     res.status(200).json(nota);
   } catch (error) {
-    res.status(500).json({ error: "Erro ao buscar nota!", message: error.message });
+    next(error);
   }
 });
 
-notaController.post("/", async (req, res) => {
+notaController.post("/", async (req, res, next) => {
   try {
     const nota = await notaService.create({ ...req.body });
     res.status(201).json(nota);
   } catch (error) {
-    res.status(500).json({ error: "Erro ao criar nota!", message: error.message });
+    next(error);
   }
 });
 
-notaController.put("/:id", async (req, res) => {
+notaController.put("/:id", async (req, res, next) => {
   try {
     const notaAtualizada = await notaService.update(req.params.id, { ...req.body });
     res.status(200).json(notaAtualizada);
   } catch (error) {
-    res.status(500).json({ error: "Erro ao atualizar nota!", message: error.message });
+    next(error);
   }
 });
 
-notaController.delete("/:id", async (req, res) => {
+notaController.delete("/:id", async (req, res, next) => {
   try {
     await notaService.delete(req.params.id);
     res.status(204).send();
   } catch (error) {
-    res.status(500).json({ error: "Erro ao deletar nota!", message: error.message });
+    next(error);
   }
 });
 
